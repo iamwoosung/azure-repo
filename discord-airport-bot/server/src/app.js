30a66380a8f8fa96d1e15b29b69b6cb8e306cba3
iamwoosung <imws.jo@gmail.com>
2024-12-20 16:43:49 +0900

    GCP Test URL 추가
    GCP Test URL 추가

--- discord-airport-bot/server/src/app.js
+++ discord-airport-bot/server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // const authRoutes = require('./routes/auth');
-require('dotenv').config(({ path: 'pkg.env.dev' }));
+require('dotenv').config(({ path: 'pkg.env.devz' }));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 // app.use('/api/auth', authRoutes);
+
+// 새로운 라우트 추가
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'hello world!' });
+});
 
 app.listen(PORT, () => {
     console.log(`listen server ${PORT} port.`);
