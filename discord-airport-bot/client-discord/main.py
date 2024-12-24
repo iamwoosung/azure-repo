2e9f7a0590a112f3eb4115edd539ce46a781f344
iamwoosung <imws.jo@gmail.com>
2024-12-24 16:48:39 +0900

    main 작성중
    main 작성중

--- discord-airport-bot/client-discord/main.py
+++ discord-airport-bot/client-discord/main.py
@@ -1,1 +1,47 @@
 import discord
+from discord.ext import commands
+
+from bot import AirportCommand
+
+from quickchart import QuickChart
+
+qc = QuickChart()
+qc.width = 500
+qc.height = 300
+qc.device_pixel_ratio = 2.0
+qc.config = {
+    "type": "bar",
+    "data": {
+        "labels": ["Hello world", "Test"],
+        "datasets": [{
+            "label": "Foo",
+            "data": [1, 2]
+        }]
+    }
+} 
+
+
+TOKEN = 
+CHANNEL_ID = 
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = '!', help_command = None, intents=intents)
+
+@bot.event
+async def on_ready():
+    # bot 인스턴스에 
+    # 커맨드 클래스 바인딩
+    await bot.add_cog(AirportCommand(bot))
+ 
+
+bot.run(TOKEN)
+
+
+
+
+# Print a chart URL
+print(qc.get_url())
+
+# Print a short chart URL
+print(qc.get_short_url())
