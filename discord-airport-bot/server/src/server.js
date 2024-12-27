1b6075f535ccf80fc66f87d5b35eff4532ff6faf
iamwoosung <imws.jo@gmail.com>
2024-12-27 20:07:50 +0900

    프로시저 결과 반환
    프로시저 결과 반환

--- discord-airport-bot/server/src/server.js
+++ discord-airport-bot/server/src/server.js
@@ -9,10 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+// 목록이 많지 않아서 라우팅, 미들웨어 처리하지 않음
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'hello world!' });
-});
+app.get('/', async (req, res) => {
+    const result = await db.doCallProcedure("CALL GET_AIRPORT_LIST()");
+    res.send(result[0][0]);
+}); 
 
 app.listen(PORT, () => {
     console.log(`listen server ${PORT} port.`);
