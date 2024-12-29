306c4df850f976c4a22548fb1a276db86478e987
iamwoosung <imws.jo@gmail.com>
2024-12-29 21:52:28 +0900

    Model 추가
    Model 추가

---
+++ discord-airport-bot/client-discord/models/airport.py
@@ -0,0 +1,11 @@
+class Airport:
+    def __init__(self, airportNo, airportCode, airportName, airportCompany, airportAddress, airportTel, airportOpenDate, airportInsideTraffic, airportParkingTraffic):
+        self.airport_no = airportNo
+        self.airport_code = airportCode
+        self.airport_name = airportName
+        self.airport_company = airportCompany
+        self.airport_address = airportAddress
+        self.airport_tel = airportTel
+        self.airport_open_date = airportOpenDate
+        self.airport_inside_traffic = airportInsideTraffic
+        self.airport_parking_traffic = airportParkingTraffic
---
+++ discord-airport-bot/client-discord/models/parking.py
@@ -0,0 +1,9 @@
+class AirportParking:
+    def __init__(self, airportParkingTrafficNo, airportName, airportParkingName, airportParking, airportParkingSpace, updateDate, insertDate):
+        self.airport_parking_traffic_no = airportParkingTrafficNo
+        self.airport_name = airportName
+        self.airport_parking_name = airportParkingName
+        self.airport_parking = airportParking
+        self.airport_parking_space = airportParkingSpace
+        self.update_date = updateDate
+        self.insert_date = insertDate
